b82e913994f97b07f1c2fa777a2ebca660bcb3a3
github username <github email address>
2019-04-02 20:08:09 +0100

    with a centred image slider changing every 3 seconds, header and background colour

---
+++ index.js
@@ -0,0 +1,26 @@
+let i = 0;
+const images = [];
+const time = 3000;
+
+images[0] = 'image1.jpg';
+images[1] = 'image2.jpg';
+images[2] = 'image3.jpg';
+images[3] = 'image4.jpg';
+images[4] = 'image5.jpg';
+images[5] = 'image6.jpg';
+
+
+function changeImage() {
+  document.slide.src = images[i];
+
+  if (i < images.length -1) {
+    i++;
+  } else {
+    i = 0;
+  }
+
+  setTimeout("changeImage()", time)
+
+}
+
+window.onload = changeImage;
